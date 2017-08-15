831dcba43eacfce626d4687f06358532ad5d99ce
Phantomias2006 <flo.riedl@gmx.at>
2017-08-15 12:59:53 +0200

    Add - DB config File

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+/* INCLUDE: Verbindung zur Text-Datenbank herstellen */
+ 
+$db_server = "localhost"; // Hostname
+$db_user = "*****"; // Benutzername
+$db_pass = "*****"; // Kennwort
+$db_name = "*****"; // Name der Datenbank
+
+?>
